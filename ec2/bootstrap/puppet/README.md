5bfb9bdc56e4d16305116d0b6f864363e040db0e
ljay79 <github@jens79.de>
2016-08-17 23:02:02 +0200

    start playing with ec2 user-data bootstrapping with puppet

---
+++ ec2/bootstrap/puppet/README.md
@@ -0,0 +1,3 @@
+# bootstrapping EC2 instances with AWS user-data
+
+http://codento.fi/2012/02/hello-ec2-part-1-bootstrapping-instances-with-cloud-init-git-and-puppet/
---
+++ ec2/bootstrap/puppet/user-data-script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#
+# sample script
+#
+
+set -e -x
+
+yum --assumeyes --quiet update
+yum --assumeyes --quiet install git puppet3
+
+#
+# Fetch puppet configuration from public git repository.
+#
+
+mv /etc/puppet /etc/puppet.orig
+git clone https://github.com/ljay79/ec2-puppet-boot.git /etc/puppet
+
+#
+# Run puppet.
+#
+
+puppet apply /etc/puppet/manifests/init.pp
